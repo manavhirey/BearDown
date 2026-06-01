ef7469302a003ae6968c1c0fbe7ad3e681a7bca3
Manav Hirey <manavhirey@gmail.com>
2026-05-31 21:12:51 -0400

    chore: scaffold Xcode project for BearDown (iOS 17, SwiftUI, CloudKit, notifications)

---
+++ BearDown/BearDown/BearDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  BearDownApp.swift
+//  BearDown
+//
+//  Created by Manav on 5/31/26.
+//
+
+import SwiftUI
+
+@main
+struct BearDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
